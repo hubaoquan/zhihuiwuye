578e4740a0f261a6df9da32604e4f98cff806c4c
胡宝全 <971281722@qq.com>
2016-09-24 16:29:57 +0800

    2016年9月24日16:25:09

---
+++ QR_control_door/src/main/java/com/test/team/activity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.test.team.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.test.team.qrscandoor.R;
+
+public class LoginActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setTitle("请登录");
+    }
+}
